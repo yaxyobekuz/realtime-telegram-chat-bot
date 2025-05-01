7c877e4e148b26ef97c9085c5863b734fa550437
yaxyobekb <pubgn9642@gmail.com>
2025-05-01 21:23:35 +0500

    Refactor message handling to utilize hooks for user and message management

--- bot/handlers/messageHandler.js
+++ bot/handlers/messageHandler.js
@@ -1,25 +1,34 @@
 const bot = require("../bot");
+const useUser = require("../../hooks/useUser");
+const useText = require("../../hooks/useText");
 const { socket } = require("../../backend/app");
+const useMessage = require("../../hooks/useMessage");
 const chats = require("../../backend/models/chatModel");
-const users = require("../../backend/models/userModel");
 const messages = require("../../backend/models/messagesModel");
-const userPhoto = require("../../backend/models/userPhotoModel");
-const { getUserProfilePhotoBuffer } = require("../utils/helpers");
 
 bot.on("message", async (msg) => {
   const message = msg.text;
   const chatId = msg.chat.id;
 
-  const user = await users.findOne({ id: chatId });
+  const { t } = useText(chatId);
+  const { reply, matches } = useMessage(chatId, message);
+  const { findUserById, registerUser, isUserInState } = useUser(chatId);
+  const user = await findUserById(); // Get user data from data base
 
   // Create user
   if (!user) {
-    const newUser = await formatUserData(msg.from);
-    return await users.create(newUser); // Add new user to the database
+    try {
+      await registerUser(msg.from);
+      return reply(t("userGreeting"));
+    } catch {
+      return reply(t("userGreeting"));
+    }
   }
 
+  const isUserReadyForMessage = isUserInState(user, "awaitingMessage");
+
   // Create chat with admin
-  if (message === "/chat" && user.status !== "awaitingMessage") {
+  if (matches("/chat") && !isUserReadyForMessage) {
     try {
       const existingChat = await chats.findOne({ id: chatId });
 
@@ -27,7 +36,6 @@ bot.on("message", async (msg) => {
       if (!existingChat) {
         const chat = await chats.create({ user, id: chatId });
         socket.emit("receiveChat", chat); // Send new chat data to chat app
-        console.log("Chat yaratildi:", chat);
       }
 
       const existingChatMessages = await messages.findOne({ id: chatId });
@@ -40,17 +48,17 @@ bot.on("message", async (msg) => {
       user.status = "awaitingMessage";
       await user.save(); // Update user status in the database
 
-      bot.sendMessage(chatId, "😊 Chat boshlandi! Iltimos, xabar yuboring: ");
+      reply("😊 Chat boshlandi! Iltimos, xabar yuboring: ");
 
       return;
     } catch (err) {
       console.log("Chat yaratishda xatolik:", err);
-      bot.sendMessage(chatId, "Chat yaratishda xatolik yuz berdi.");
+      reply("Chat yaratishda xatolik yuz berdi.");
     }
   }
 
   // Send messages to admin
-  if (user.status === "awaitingMessage") {
+  if (isUserReadyForMessage) {
     if (!message) return; // Check if message is not empty
 
     try {
@@ -58,10 +66,7 @@ bot.on("message", async (msg) => {
 
       // Check if chat exists
       if (!chatMessages) {
-        bot.sendMessage(
-          chatId,
-          "Chat topilmadi. Iltimos, /chat buyrug'ini yuboring."
-        );
+        reply("Chat topilmadi. Iltimos, /chat buyrug'ini yuboring.");
 
         user.status = "default"; // Reset user status
         await user.save(); // Update user status in the database
@@ -70,37 +75,13 @@ bot.on("message", async (msg) => {
 
       chatMessages.messages.push({ text: message });
       await chatMessages.save();
-      bot.sendMessage(chatId, "Xabar muvaffaqiyatli yuborildi!");
+      reply("Xabar muvaffaqiyatli yuborildi!");
 
       // Send new message data to chat app
       socket.emit(`chatMessage:${chatId}`, { text: message, type: "text" });
     } catch (err) {
-      console.error("Xabar saqlashda xatolik:", err);
-      bot.sendMessage(chatId, "Xabarni saqlab bo'lmadi.");
+      console.log("Xabar saqlashda xatolik: ", err);
+      reply("Xabarni saqlab bo'lmadi.");
     }
   }
 });
-
-async function formatUserData(user) {
-  let photoUrl = null;
-  const photo = await getUserProfilePhotoBuffer(user.id);
-
-  if (photo) {
-    await userPhoto
-      .create({
-        id: user.id,
-        data: photo.buffer,
-        contentType: photo.contentType,
-      })
-      .then(() => {
-        photoUrl = `/api/profile-photo/${user.id}`;
-      });
-  }
-
-  return {
-    id: user.id,
-    photo: photoUrl,
-    username: user.username,
-    firstName: user.first_name || null,
-  };
-}
