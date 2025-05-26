b5d3c53117a8ae0aea0b247f6f3412ac13524078
yaxyobekb <pubgn9642@gmail.com>
2025-05-26 17:36:53 +0500

    Add chat statuses data

---
+++ data/statuses.js
@@ -0,0 +1,52 @@
+const statuses = [
+  {
+    label: "Yangi",
+    value: "new",
+  },
+  {
+    label: "Olishi mumkin",
+    value: "canBeTaken",
+  },
+  {
+    label: "Pasport kutilmoqda",
+    value: "waitingForPassport",
+  },
+  {
+    label: "To'lov kutilmoqda",
+    value: "waitingForPayment",
+  },
+  {
+    label: "Chipta berilmoqda",
+    value: "ticketBeingIssued",
+  },
+  {
+    label: "Chipta berilgan",
+    value: "ticketIssued",
+  },
+  {
+    label: "Bloklangan",
+    value: "blocked",
+  },
+  {
+    label: "Bekor qilingan",
+    value: "cancelled",
+  },
+  {
+    label: "Rad etilgan",
+    value: "rejected",
+  },
+  {
+    label: "Ko'rib chiqilmoqda",
+    value: "underReview",
+  },
+  {
+    label: "Yakunlangan",
+    value: "completed",
+  },
+  {
+    label: "Yetkazilgan",
+    value: "delivered",
+  },
+];
+
+module.exports = statuses;
