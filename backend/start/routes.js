61441b0c87dcefbc27b03f163bf47f21e93c2bce
yaxyobekb <pubgn9642@gmail.com>
2025-05-30 21:19:17 +0500

    Remove messages route from the application

--- backend/start/routes.js
+++ backend/start/routes.js
@@ -1,7 +1,5 @@
 const { app, express } = require("../app");
 const chatsRoute = require("../routes/chatsRoute");
-const messagesRoute = require("../routes/messagesRoute");
 
 app.use(express.json());
 app.use("/api/chats", chatsRoute);
-app.use("/api/messages", messagesRoute);
