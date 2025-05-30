cd289f41f219950235980338d1e0ec1c4bbb5779
yaxyobekb <pubgn9642@gmail.com>
2025-05-30 21:19:44 +0500

    Remove unused messages model and related initialization code

--- backend/index.js
+++ backend/index.js
@@ -1,28 +1,9 @@
 require("./db/connectDB");
 const { server } = require("./app");
 const { port } = require("../config");
-const messagesModel = require("./models/messagesModel");
 
 require("./socket");
 require("./start/routes");
-
-(async () => {
-  const savedMessages = await messagesModel.findOne({ id: 1 });
-
-  if (savedMessages) return;
-
-  const createdSavedMessages = new messagesModel({
-    id: 1,
-    messages: [],
-    user: { id: 1, username: null },
-  });
-
-  try {
-    await createdSavedMessages.save();
-  } catch {
-    console.log("Saved messages error! ", savedMessages);
-  }
-})();
 
 server.listen(port, () => {
   console.log(`Server http://localhost:${port} da ishga tushdi!`);
