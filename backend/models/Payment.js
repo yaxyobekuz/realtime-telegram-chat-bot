5813b05f24b92f0a7d5d5f53dc02276c6186096f
yaxyobekb <pubgn9642@gmail.com>
2025-06-02 12:19:25 +0500

    Add payment model

---
+++ backend/models/Payment.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const paymentSchema = new mongoose.Schema({
+  amount: { type: Number, required: true },
+  chatId: { type: Number, required: true },
+  messageId: { type: String, required: true },
+  description: { type: String, default: null },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  photo: { type: mongoose.Schema.Types.ObjectId, ref: "Photo", required: true },
+});
+
+module.exports = mongoose.model("Payment", paymentSchema);
