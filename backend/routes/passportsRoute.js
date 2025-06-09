926db716dcc17d3e7a2cf6eda7cd42bbd94a4f6d
yaxyobekb <pubgn9642@gmail.com>
2025-06-09 17:02:06 +0500

    Add passports route and implement CRUD operations for passport management

---
+++ backend/routes/passportsRoute.js
@@ -0,0 +1,100 @@
+const express = require("express");
+const router = express.Router();
+
+// Models
+const User = require("../models/User");
+const Photo = require("../models/Photo");
+const Message = require("../models/Message");
+const Passport = require("../models/Passport");
+
+// Get all passports
+router.get("/", async (req, res) => {
+  try {
+    const passports = await Passport.find().populate("user").populate("photo");
+    res.send(passports);
+  } catch (error) {
+    res.status(500).send("Ichki xatolik");
+  }
+});
+
+// Get single passport
+router.get("/passport/:id", async (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({ message: "Pasport ID raqami mavjud emas" });
+  }
+
+  try {
+    const passport = await Passport.findById(id)
+      .populate("user")
+      .populate("photo");
+
+    if (!passport) {
+      return res
+        .status(404)
+        .json({ message: "Pasport ma'lumotlari topilmadi" });
+    }
+
+    res.send(passport);
+  } catch (error) {
+    res.status(500).send("Ichki xatolik");
+  }
+});
+
+// Create a new passport
+router.post("/new", async (req, res) => {
+  const { chatId, messageId, description, userId, photoId } = req.body;
+
+  if (!chatId || !messageId || !userId || !photoId) {
+    return res
+      .status(400)
+      .json({ message: "Talab qilingan ma'lumotlar to'liq emas" });
+  }
+
+  try {
+    const existingPassport = await Passport.findOne({ messageId });
+
+    if (existingPassport) {
+      return res.status(400).json({
+        message: "Xabar allaqachon Pasport ma'lumotlari sifatida saqlangan",
+      });
+    }
+
+    const user = await User.findOne({ id: userId });
+
+    if (!user) {
+      return res.status(404).json({ message: "Foydalanuvchi topilmadi" });
+    }
+
+    const photo = await Photo.findOne({ _id: photoId });
+
+    if (!photo) {
+      return res.status(404).json({ message: "Rasm topilmadi" });
+    }
+
+    const newPassport = await Passport.insertOne({
+      chatId,
+      messageId,
+      description,
+      user: user._id,
+      photo: photo._id,
+    });
+
+    await Message.findByIdAndUpdate(
+      { _id: messageId },
+      { passportId: newPassport._id }
+    );
+
+    res.status(201).send({
+      ok: true,
+      data: newPassport,
+      message: "Pasport muvaffaqiyatli yaratildi",
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Ichki xatolik");
+  }
+});
+
+module.exports = router;
--- backend/start/routes.js
+++ backend/start/routes.js
@@ -1,7 +1,9 @@
 const { app, express } = require("../app");
 const chatsRoute = require("../routes/chatsRoute");
 const paymentsRoute = require("../routes/paymentsRoute");
+const passportsRoute = require("../routes/passportsRoute");
 
 app.use(express.json());
 app.use("/api/chats", chatsRoute);
 app.use("/api/payments", paymentsRoute);
+app.use("/api/passports", passportsRoute);
